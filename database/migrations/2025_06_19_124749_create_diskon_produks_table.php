2c0f4f11d62f9f0fa1484de67790ff098568369b
safarqwerty <safar.alam79@gmail.com>
2025-07-13 22:15:22 +0800

    hapus relasi di migrasi

--- database/migrations/2025_06_19_124749_create_diskon_produks_table.php
+++ database/migrations/2025_06_19_124749_create_diskon_produks_table.php
@@ -14,19 +14,11 @@ return new class extends Migration
         Schema::create('diskon_produks', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid');
-            $table->uuid('uuid_produk');
+            $table->uuid('uuid_produk'); // Kolom ini akan menjadi kunci relasi
             $table->unsignedTinyInteger('diskon_persen')->nullable();
             $table->dateTime('akhir_tanggal');
             $table->timestamps();
-
-            // Pastikan index ada sebelum foreign key
             $table->index('uuid_produk');
-
-            // Tambahkan foreign key constraint
-            $table->foreign('uuid_produk')
-                  ->references('uuid')
-                  ->on('products')
-                  ->onDelete('cascade');
         });
     }
 
